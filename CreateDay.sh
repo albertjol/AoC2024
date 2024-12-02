e494b254cb23df134fd312a5c0a09bb05a7733f4
Albert Jol <albertjol@gmail.com>
2024-12-02 21:22:58 +0100

    Initial day 2

--- CreateDay.sh
+++ CreateDay.sh
@@ -5,7 +5,11 @@ mv AdvCodeTemplate.lpi AdvCode$1.lpi
 mv AdvCodeTemplate.lpr AdvCode$1.lpr
 mv uAdvCodeTemplate.lfm uAdvCode$1.lfm
 mv uAdvCodeTemplate.pas uAdvCode$1.pas
-sed -i '' -e "s/AdvCodeTemplate/AdvCode$1/g" *
 mv AdvCodeTemplate.ico AdvCode$1.ico
+
+# Support for !(*.ico)
+shopt -s extglob
+sed -i '' -e "s/AdvCodeTemplate/AdvCode$1/g" !(*.ico)
+
 cd ..
 echo AdvCode$1 >> .gitignore
