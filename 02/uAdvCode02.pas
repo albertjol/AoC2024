43adf74989aadeb6bfcd6710ca0fea218f93aeba
Albert Jol <albertjol@florinco.nl>
2024-12-03 20:57:05 +0100

    Day 2

--- 02/uAdvCode02.pas
+++ 02/uAdvCode02.pas
@@ -12,6 +12,8 @@ uses
 
 type
 
+  TIntlist = TList<Integer>;
+
   { TFrmAdvCode }
 
   TFrmAdvCode = class(TForm)
@@ -86,13 +88,121 @@ begin
 end;
 
 procedure TFrmAdvCode.part1(lines: TStringList);
+var
+  line, numStr: String;
+  countUnsafe: Integer = 0;
+  sign: Integer;
+  prev: Integer;
+  num: Integer;
+  diff: Integer;
 begin
-  ed_Answer1.Text := IntToStr(999);
+  for line in lines do
+  begin
+    sign := 0;
+    prev := -1;
+    for numStr in line.Split(' ') do
+    begin
+      num := StrToInt(numStr);
+      if (prev >= 0) then
+      begin
+        diff := num - prev;
+
+        if (sign = 0) then
+        begin
+          sign := diff;
+        end;
+
+        if ((Abs(diff) = 0) or (Abs(diff) > 3) or (sign * diff < 0)) then
+        begin
+          Inc(countUnsafe);
+          break;
+        end;
+      end;
+      prev := num;
+    end;
+  end;
+  ed_Answer1.Text := IntToStr(lines.Count - countUnsafe);
 end;
 
 procedure TFrmAdvCode.part2(lines: TStringList);
+  function LineIsUnsafe(line: TList<Integer>): Boolean;
+  var
+    sign: Integer;
+    prev: Integer;
+    num: Integer;
+    diff: Integer;
+  begin
+    Result := False;
+    sign := 0;
+    prev := -1;
+    for num in line do
+    begin
+      if (prev >= 0) then
+      begin
+        diff := num - prev;
+
+        if (sign = 0) then
+        begin
+          sign := diff;
+        end;
+
+        if ((Abs(diff) = 0) or (Abs(diff) > 3) or (sign * diff < 0)) then
+        begin
+          Result := True;
+          break;
+        end;
+      end;
+      prev := num;
+    end;
+  end;
+
+var
+  line: String;
+  split: TStringArray;
+  countSafe: Integer = 0;
+  dl: TIntlist;
+  dampenedLines: TObjectList<TIntlist>;
+
+  procedure MakeDampenedLines(splittedLine: TStringArray);
+  var
+    i, j: Integer;
+    newLine: TIntlist;
+  begin
+    // The original line is not relevant anymore.
+    dampenedLines.Clear();
+    for i := 0 to Length(splittedLine) - 1 do
+    begin
+      newLine := TIntlist.Create();
+      dampenedLines.Add(newLine);
+      for j := 0 to Length(splittedLine) - 1 do
+      begin
+        if i <> j then
+          // Skip one of the elements.
+          newline.Add(StrToInt(splittedLine[j]));
+      end;
+    end;
+  end;
+
 begin
-  ed_Answer2.Text := IntToStr(999);
+  dampenedLines := TObjectList<TIntlist>.Create();
+  try
+    for line in lines do
+    begin
+      split := line.Split(' ');
+      MakeDampenedLines(split);
+      for dl in dampenedLines do
+      begin
+        if (not LineIsUnsafe(dl)) then
+        begin
+          Inc(countSafe);
+          break;
+        end;
+      end;
+    end;
+  finally
+    FreeAndNil(dampenedLines);
+  end;
+  ed_Answer2.Text := IntToStr(countSafe);
 end;
 
 end.
