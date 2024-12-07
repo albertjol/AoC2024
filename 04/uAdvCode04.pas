79d8f5d26b3698ce37a3cc7daf60d3a778d711c8
Albert Jol <albertjol@florinco.nl>
2024-12-07 22:32:56 +0100

    Day 4

--- 04/uAdvCode04.pas
+++ 04/uAdvCode04.pas
@@ -86,13 +86,153 @@ begin
 end;
 
 procedure TFrmAdvCode.part1(lines: TStringList);
+var
+  line: String;
+  count: Integer = 0;
+  i,j: Integer;
+  u,v: Integer;
+  newLine: String = '';
+  totalLines: TStringList;
 begin
-  ed_Answer1.Text := IntToStr(999);
+  // Okay, this works but it is way to complex....
+  SetLength(newLine, lines[0].Length);
+  totalLines := TStringList.Create();
+  try
+      // Add original
+    totalLines.AddStrings(lines);
+
+    // Add vertical
+    SetLength(newLine, lines.Count);
+    for i := 1 to lines[0].Length do
+    begin
+      for j := 0 to lines.Count - 1 do
+      begin
+        newLine[j+1] := lines[j][i];
+      end;
+      totalLines.Add(newLine);
+    end;
+
+    // The diagonals...
+    // This is rather interesting as the size varies...
+    // Strings are 1-based
+
+    // descending
+    // Start at [Y_n-4][1], [Y_n-5][1] .. [0][1], [0][2] .. [0][X_n-3]
+    // Part 1
+    for i := lines.Count - 4 downto 0 do
+    begin
+      SetLength(newLine, lines.Count - i);
+      for j := 1 to lines.Count - i do
+      begin
+        u := i-1+j;
+        v := j;
+        newLine[j] := lines[u][v];
+      end;
+      totalLines.Add(newLine);
+    end;
+
+    // Part 2
+    for i := 2 to lines[0].Length - 3 do
+    begin
+      SetLength(newLine, lines[0].Length + 1 - i);
+      for j := 1 to lines[0].Length - i + 1 do
+      begin
+        u := j-1;
+        v := i+j-1;
+        newLine[j] := lines[u][v];
+      end;
+      totalLines.Add(newLine);
+    end;
+
+    // ascending
+    // Start at [3][1], [4][1] .. [Y_n-1][1], [Y_n-1][2] .. [Y_n-1][X_n-3]
+    // Part 1
+    for i := 3 to lines.Count - 1 do
+    begin
+      SetLength(newLine, i+1);
+      for j := 1 to i + 1 do
+      begin
+        u := i+1-j;
+        v := j;
+        newLine[j] := lines[u][v];
+      end;
+      totalLines.Add(newLine);
+    end;
+
+    // Part 2
+    // [Y_n-1][2] .. [Y_n-1][X_n-3]
+    for i := 2 to lines[0].Length - 3 do
+    begin
+      SetLength(newLine, lines[0].Length - i + 1);
+      for j := 1 to lines[0].Length - i + 1 do
+      begin
+        u := lines.Count-j;
+        v := i+j-1;
+        newLine[j] := lines[u][v];//[0][1];
+      end;
+      totalLines.Add(newLine);
+    end;
+
+    for line in totalLines do
+    begin
+      i := 0;
+      repeat
+        i := line.IndexOf('XMAS', i) + 1;
+        if i > 0 then
+          Inc(count);
+      until i = 0;
+      i := 0;
+      repeat
+        i := line.IndexOf('SAMX', i) + 1;
+        if i > 0 then
+          Inc(count);
+      until i = 0;
+    end;
+    ed_Answer1.Text := IntToStr(count);
+  finally
+    FreeAndNil(totalLines);
+  end;
 end;
 
 procedure TFrmAdvCode.part2(lines: TStringList);
+var
+  count: Integer = 0;
+  i, j: Integer;
+
+  function masAscending(): Boolean;
+  begin
+    case lines[i+1][j-1] of
+      'M': Result := lines[i-1][j+1] = 'S';
+      'S': Result := lines[i-1][j+1] = 'M';
+    else
+      Result := False;
+    end;
+  end;
+
+  function masDescending(): Boolean;
+  begin
+    case lines[i-1][j-1] of
+      'M': Result := lines[i+1][j+1] = 'S';
+      'S': Result := lines[i+1][j+1] = 'M';
+    else
+      Result := False;
+    end;
+  end;
+
 begin
-  ed_Answer2.Text := IntToStr(999);
+  // OK. Just find a A in the center part of the grid.
+  for i := 1 to lines.Count - 2 do
+  begin
+    for j := 2 to lines[i].Length - 1 do
+    begin
+      if lines[i][j] = 'A' then
+      begin
+        if masAscending() and masDescending() then
+          Inc(count);
+      end;
+    end;
+  end;
+  ed_Answer2.Text := IntToStr(count);
 end;
 
 end.
