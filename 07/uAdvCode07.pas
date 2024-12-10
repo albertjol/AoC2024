d0eb43fbf2e5a9654c418d30e6a865d05f6ce1ba
Albert Jol <albertjol@florinco.nl>
2024-12-10 22:43:50 +0100

    Day 7 added timing

--- 07/uAdvCode07.pas
+++ 07/uAdvCode07.pas
@@ -22,6 +22,8 @@ type
     lbl_Answer2: TLabel;
     lbl_Filename: TLabel;
     lbl_Answer1: TLabel;
+    lbl_duration1: TLabel;
+    lbl_duration2: TLabel;
 
     procedure btn_StartClick(Sender: TObject);
     procedure FormCreate(Sender: TObject);
@@ -47,8 +49,24 @@ uses
 { TFrmAdvCode }
 
 procedure TFrmAdvCode.btn_StartClick(Sender: TObject);
+
+  function FormatSeparated(number: QWord): String;
+  var
+    i, j: SizeInt;
+  begin
+    Result := IntToStr(number);
+    j := 0;
+    for i := Result.Length downto 1 do
+    begin
+      if (j > 0) and ((j mod 3) = 0) then
+        Result.Insert(i, ' ');
+      Inc(j);
+    end;
+  end;
+
 var
   lines: TStringList;
+  ms: QWord;
 begin
   with TStringList.Create() do
   begin
@@ -62,8 +80,13 @@ begin
   lines := TStringList.Create();
   try
     lines.LoadFromFile(ed_Filename.Text);
+    ms := GetTickCount64();
     part1(lines);
+    lbl_duration1.Caption := 'Duration of part 1 (ms): ' + FormatSeparated(GetTickCount64() - ms);
+    Application.ProcessMessages();
+    ms := GetTickCount64();
     part2(lines);
+    lbl_duration2.Caption := 'Duration of part 2 (ms): ' + FormatSeparated(GetTickCount64() - ms);
   finally
     FreeAndNil(lines);
   end;
