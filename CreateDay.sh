3c7be9653f2d082537cb3e078a720aa2241fbb18
Albert Jol <albertjol@florinco.nl>
2024-12-16 21:51:56 +0100

    trivial changes

--- CreateDay.sh
+++ CreateDay.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-cp -R Template $1
-cd $1
-mv AdvCodeTemplate.lpi AdvCode$1.lpi
-mv AdvCodeTemplate.lpr AdvCode$1.lpr
-mv uAdvCodeTemplate.lfm uAdvCode$1.lfm
-mv uAdvCodeTemplate.pas uAdvCode$1.pas
-mv AdvCodeTemplate.ico AdvCode$1.ico
+read -p 'Daynumber: ' day
+
+cp -R Template $day
+cd $day
+mv AdvCodeTemplate.lpi AdvCode$day.lpi
+mv AdvCodeTemplate.lpr AdvCode$day.lpr
+mv uAdvCodeTemplate.lfm uAdvCode$day.lfm
+mv uAdvCodeTemplate.pas uAdvCode$day.pas
+mv AdvCodeTemplate.ico AdvCode$day.ico
 
 # Support for !(*.ico)
 shopt -s extglob
-sed -i '' -e "s/AdvCodeTemplate/AdvCode$1/g" !(*.ico)
+sed -i '' -e "s/AdvCodeTemplate/AdvCode$day/g" !(*.ico)
 
 cd ..
-echo AdvCode$1 >> .gitignore
+echo AdvCode$day >> .gitignore
